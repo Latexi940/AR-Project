191cae8334c555603b7ec9b9c5ff939a6581d4bf
laurinis <lauri.nissinen@metropolia.fi>
2020-09-22 11:39:18 +0300

    Gets user location and restricts monster spawning if user does not move enough

--- app/src/main/java/com/example/ar_project/MainActivity.kt
+++ app/src/main/java/com/example/ar_project/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.ar_project
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +13,7 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
@@ -16,10 +23,13 @@ import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     private lateinit var fragment: ArFragment
     private var testRenderable: ModelRenderable? = null
+    private var userLocation: Location? = null
+    private var spawningLocation: Location? = null
+    private var distanceTravelled = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +58,32 @@ class MainActivity : AppCompatActivity() {
         fragment.arSceneView.scene.addOnUpdateListener {
             frameUpdate()
         }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                0
+            )
+        }
+
+        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0f, this)
     }
 
     private fun frameUpdate() {
@@ -60,7 +96,9 @@ class MainActivity : AppCompatActivity() {
             for (hit in hits) {
                 val trackable = hit.trackable
                 if (trackable is Plane) {
-                    if (getSpawningChance(1000)) {
+                    if (getSpawningChance(100) && isSpawningAllowed()) {
+                        Log.i("ARPROJECT", "Spawn!")
+                        spawningLocation = userLocation
                         val anchor = hit!!.createAnchor()
                         val anchorNode = AnchorNode(anchor)
                         anchorNode.setParent(fragment.arSceneView.scene)
@@ -88,11 +126,31 @@ class MainActivity : AppCompatActivity() {
 
     private fun getSpawningChance(chance: Int): Boolean {
         val randomNumber = (0..chance).random()
-        return if (randomNumber == 1) {
-            Log.i("ARPROJECT", "Spawn!")
-            true
-        } else {
-            false
+        return randomNumber == 1
+    }
+
+    private fun isSpawningAllowed(): Boolean{
+        if(spawningLocation == null){
+            return true
         }
+        (return userLocation?.distanceTo(spawningLocation)!! > 50)
+    }
+
+    override fun onLocationChanged(currentLocation: Location) {
+
+        if(userLocation != null){
+            distanceTravelled += currentLocation.distanceTo(userLocation)
+        }
+
+        userLocation = currentLocation
+    }
+
+    override fun onStatusChanged(p: String?, p1: Int, p2: Bundle?) {
+    }
+
+    override fun onProviderEnabled(p: String?) {
+    }
+
+    override fun onProviderDisabled(p: String?) {
     }
 }
