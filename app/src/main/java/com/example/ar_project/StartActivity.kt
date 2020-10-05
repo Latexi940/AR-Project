2c55479e001c54558bf101de16eb0e7841ddfea4
laurinis <lauri.nissinen@metropolia.fi>
2020-10-05 13:25:39 +0300

    Fixed formatting

--- app/src/main/java/com/example/ar_project/StartActivity.kt
+++ app/src/main/java/com/example/ar_project/StartActivity.kt
@@ -11,7 +11,6 @@ import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
